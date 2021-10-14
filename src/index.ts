3853fd0aa95a88e1d35c6a684a1e964b24334c68
Gijs Nieuwenhuis <gijs@tapraise.com>
2021-10-14 06:49:00 +0200

    Prepare for npm publishing

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default } from './transformer/EntityToObjectTransformer';
+
+export { default as CouldNotAssignPropertyValueError} from './error/CouldNotAssignPropertyValueError';
+export { default as PropertiesNotMappedError} from './error/PropertiesNotMappedError';
+export { default as PropertyNotFoundInObjectError} from './error/PropertyNotFoundInObjectError';
+export { default as PropertyNotFoundOnEntityError} from './error/PropertyNotFoundOnEntityError';
